c01341f5c4c9f29b6301ad15c69abd7dd3f7e8d9
belledon <mbelledonne@gmail.com>
2020-10-09 16:50:59 -0400

    new window design

--- attention.r
+++ attention.r
@@ -20,44 +20,50 @@ full_data <- attention_full %>%
                values_to = "att")  %>%
   separate(tracker, c(NA, "tracker"), sep = '_') %>%
   mutate(tracker = as.numeric(tracker)) %>%
-  filter(frame > 24)
+  filter(frame > 48)
 
 tps <- full_data %>%
   group_by(scene, tracker) %>%
   mutate(t_max = which.max(att) + min(frame),
          delta_t = frame - t_max) %>%
   group_by(scene) %>%
-  filter(between(delta_t, -10, 10)) %>%
-  group_by(scene) %>%
+  filter(between(delta_t, -12, 12)) %>%
+  group_by(scene, tracker) %>%
   mutate(n_t = dplyr::n()) %>%
-  filter(n_t == 4 * 21) %>%
+  ungroup() %>%
+  filter(n_t >= 24) %>%
   arrange(scene, tracker)
 
-n_scenes = 24
+n_scenes = 12
 
 n_unique_scenes = length(unique(tps$scene))
-top_trials <- tps %>%
+top_trackers <- tps %>%
   group_by(scene,tracker) %>%
   mutate(max_att = max(att)) %>%
   group_by(scene) %>%
-  mutate(sd_max_att = sd(max_att),
+  mutate(scene_max = max(max_att),
+         sd_max_att = sd(max_att),
          tracker_rank = dense_rank(max_att)) %>%
+  filter(tracker_rank == max(tracker_rank)) %>%
   ungroup() %>%
-  mutate(sd_rank = dense_rank(sd_max_att)) %>%
-  filter(sd_rank > n_unique_scenes - n_scenes & tracker_rank >= 1)
+  mutate(sd_rank = dense_rank(scene_max)) %>%
+  filter(sd_rank > n_unique_scenes - n_scenes)
 
+top_trials <- top_trackers %>%
+  left_join(attention_full)
+ 
 top_trials %>%
   ggplot(aes(x = delta_t, y = att, color = factor(tracker))) +
   geom_line() +
   facet_wrap(vars(scene))
 
 fits <- top_trials %>%
-  nest_by(scene, tracker) %>%
+  nest_by(scene) %>%
   mutate(mod = list(nlsLM( att ~ k*exp(-1/2*(delta_t -mu)^2/sigma^2), start=c(mu=0,sigma=3,k=5),
                       data = data)))
 coefs <- fits %>% 
   summarise(broom::tidy(mod)) %>%
-  dplyr::select(scene, tracker, term, estimate) %>%
+  dplyr::select(scene, term, estimate) %>%
   filter(term == "sigma") %>%
   select(-term) %>%
   transmute(sigma = clamp(sqrt(estimate), 2, 3.33))
@@ -76,7 +82,7 @@ att_map <- top_trials %>%
                names_to = "epoch",
                values_to = "t") %>%
   mutate(frame = round(t)) %>% 
-  select(scene, tracker, epoch, frame, tracker_rank) %>%
+  # select(scene, tracker, epoch, frame, tracker_rank) %>%
   left_join(full_data)
 
 att_map %>%
@@ -99,8 +105,6 @@ full_data %>%
 facet_wrap(vars(scene))
 # ggsave("output/attention_trial_tps.png")
 
-
-
-# write.csv(att_map, row.names = FALSE,
-      # file = "output/exp0_probe_map.csv")
+write.csv(att_map, row.names = FALSE,
+          file = "output/isr_inertia_probe_map.csv")
 
