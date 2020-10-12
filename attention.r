9997e58df7d8485f60c7e7f8fb2e3faf9b35eafe
belledon <mbelledonne@gmail.com>
2020-10-12 18:33:51 -0400

    time aligned probes

--- attention.r
+++ attention.r
@@ -19,45 +19,46 @@ full_data <- attention_full %>%
   pivot_longer(-c(frame, scene), names_to = "tracker", 
                values_to = "att")  %>%
   separate(tracker, c(NA, "tracker"), sep = '_') %>%
-  mutate(tracker = as.numeric(tracker)) %>%
-  filter(frame > 48)
+  mutate(tracker = as.numeric(tracker))
+
+window_size = 5
 
 tps <- full_data %>%
+  filter(between(frame, 48, 96)) %>%
   group_by(scene, tracker) %>%
   mutate(t_max = which.max(att) + min(frame),
          delta_t = frame - t_max) %>%
   group_by(scene) %>%
-  filter(between(delta_t, -12, 12)) %>%
-  group_by(scene, tracker) %>%
+  filter(between(delta_t, -window_size, window_size)) %>%
   mutate(n_t = dplyr::n()) %>%
   ungroup() %>%
-  filter(n_t >= 24) %>%
+  filter(n_t >= 4*2*window_size) %>%
   arrange(scene, tracker)
 
 n_scenes = 12
 
 n_unique_scenes = length(unique(tps$scene))
-top_trackers <- tps %>%
+top_trials <- tps %>%
   group_by(scene,tracker) %>%
   mutate(max_att = max(att)) %>%
   group_by(scene) %>%
   mutate(scene_max = max(max_att),
          sd_max_att = sd(max_att),
          tracker_rank = dense_rank(max_att)) %>%
-  filter(tracker_rank == max(tracker_rank)) %>%
   ungroup() %>%
-  mutate(sd_rank = dense_rank(scene_max)) %>%
-  filter(sd_rank > n_unique_scenes - n_scenes)
+  mutate(s_rank = dense_rank(scene_max)) %>%
+  filter(s_rank > n_unique_scenes - n_scenes)
 
-top_trials <- top_trackers %>%
-  left_join(attention_full)
- 
+# top_trials <- top_trackers %>%
+#   left_join(attention_full)
+#  
 top_trials %>%
-  ggplot(aes(x = delta_t, y = att, color = factor(tracker))) +
+  ggplot(aes(x = delta_t, y = att, color = factor(tracker_rank))) +
   geom_line() +
   facet_wrap(vars(scene))
 
 fits <- top_trials %>%
+  filter(tracker_rank == max(tracker_rank)) %>%
   nest_by(scene) %>%
   mutate(mod = list(nlsLM( att ~ k*exp(-1/2*(delta_t -mu)^2/sigma^2), start=c(mu=0,sigma=3,k=5),
                       data = data)))
@@ -66,11 +67,11 @@ coefs <- fits %>%
   dplyr::select(scene, term, estimate) %>%
   filter(term == "sigma") %>%
   select(-term) %>%
-  transmute(sigma = clamp(sqrt(estimate), 2, 3.33))
+  transmute(sigma = clamp(sqrt(estimate), 2, 10.0))
  
 att_map <- top_trials %>% 
-  left_join(coefs) %>%
-  filter(delta_t == 0) %>%
+  right_join(coefs) %>%
+  filter(delta_t == 0 & tracker_rank == max(tracker_rank)) %>%
   mutate(t_1 = -3 * sigma + frame,
          t_2 = -1.5 * sigma + frame,
          t_3 = 0 * sigma + frame,
@@ -82,11 +83,15 @@ att_map <- top_trials %>%
                names_to = "epoch",
                values_to = "t") %>%
   mutate(frame = round(t)) %>% 
-  # select(scene, tracker, epoch, frame, tracker_rank) %>%
-  left_join(full_data)
+  select(scene, frame) %>%
+  left_join(full_data) %>%
+  group_by(scene) %>%
+  mutate(epoch = dense_rank(frame)) %>%
+  ungroup() %>%
+  arrange(scene, tracker)
 
 att_map %>%
-  ggplot(aes(x = factor(epoch), y = att, color = factor(tracker))) +
+  ggplot(aes(x = epoch, y = att, color = factor(tracker))) +
   geom_point() +
   facet_wrap(vars(scene))
 
