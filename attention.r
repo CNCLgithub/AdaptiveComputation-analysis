b8078bb7800c76ac7a4d3189a217dc8745b0dff2
eivinasbutkus <eivinas.butkus@yale.edu>
2020-12-17 19:09:14 -0500

    random probe placement done

--- attention.r
+++ attention.r
@@ -11,45 +11,57 @@ library(pracma)
 th <- theme_classic()
 theme_set(th)
 
-attention_full <- read_csv("data/isr_inertia_extended_target_designation_attention.csv") %>%
-rename(scene = trial) %>%
-mutate(frame = t + 2) %>%
-select(-t)
-
+attention_full <- read_csv("data/isr_inertia_480_attention.csv") %>%
+  rename(scene = trial) %>%
+  mutate(frame = t) %>%
+  select(-t)
 
 full_data <- attention_full %>%
-pivot_longer(-c(frame, scene), names_to = "tracker", 
-             values_to = "att")  %>%
-separate(tracker, c(NA, "tracker"), sep = '_') %>%
-mutate(tracker = as.numeric(tracker),
-       zatt = scale(att))
+  pivot_longer(-c(frame, scene), names_to = "tracker", 
+               values_to = "att")  %>%
+  separate(tracker, c(NA, "tracker"), sep = '_') %>%
+  mutate(tracker = as.numeric(tracker),
+         zatt = scale(att))
 
 probe_limit = 48
-window_size = 12
-max_peaks = 3
 tmax = max(full_data$frame)
 n_trackers = length(unique(full_data$tracker))
 
-scene_peaks <- function(att) {
-matt <- findpeaks(att, npeaks = max_peaks, minpeakheight = 1,
-                  minpeakdistance = window_size * n_trackers)
-return(matt[, 2])
+
+sample_timepoints <- function(probe_limit, tmax) {
+  while (TRUE) {
+    possible_frames = probe_limit:tmax-probe_limit
+    samples = sort(sample(possible_frames, 4))
+    if (all(sort(dist(samples)) > probe_limit)) {
+      return(samples + probe_limit)
+    }
+  }
 }
-# take the highs for each scene with at least some buffer between
+
 tps <- full_data %>%
-filter(between(frame, probe_limit, tmax-probe_limit)) %>%
-nest_by(scene) %>%
-mutate(frame = list(scene_peaks(data$att))) %>%
-select(-data) %>%
-unnest(cols = c(frame)) %>%
-mutate(frame = round(frame / n_trackers) + probe_limit) %>%
-left_join(full_data) %>%
-arrange(scene, frame)
+  nest_by(scene) %>%
+  mutate(frame = list(sample_timepoints(probe_limit, tmax)),
+         tracker = list(sample(4, replace = TRUE))) %>%
+  select(-data) %>%
+  unnest(cols = c(frame, tracker)) %>%
+  left_join(full_data) %>%
+  arrange(scene, frame)
+
+# # take the highs for each scene with at least some buffer between
+# tps <- full_data %>%
+#   filter(between(frame, probe_limit, tmax-probe_limit)) %>%
+#   nest_by(scene) %>%
+#   mutate(frame = list(scene_peaks(data$att))) %>%
+#   select(-data) %>%
+#   unnest(cols = c(frame)) %>%
+#   mutate(frame = round(frame / n_trackers) + probe_limit) %>%
+#   left_join(full_data) %>%
+#   arrange(scene, frame)
 
 
 scene_bd <- tps %>%
-group_by(scene, frame) %>%
-summarise(att = max(att))
+  group_by(scene, frame) %>%
+  summarise(att = max(att))
 
 
 scene_bd %>%
@@ -67,18 +79,18 @@ facet_wrap(vars(scene))
 # ggsave("output/attention_trial_tps.png")
 
 att_map <- tps %>%
-group_by(scene, frame) %>%
-mutate(att_rank = dense_rank(att)) %>%
-ungroup() %>%
-filter(att_rank== 1 | att_rank == 4) %>%
-arrange(scene, frame, att)
+  group_by(scene, frame) %>%
+  mutate(att_rank = dense_rank(att)) %>%
+  ungroup() %>%
+  filter(att_rank== 1 | att_rank == 4) %>%
+  arrange(scene, frame, att)
 
 att_map %>%
-ggplot(aes(att)) +
-xlim(0, 13) +
-geom_density(binwidth = 0.3)
+  ggplot(aes(att)) +
+  xlim(0, 13) +
+  geom_density(binwidth = 0.3)
 
 
 write.csv(att_map, row.names = FALSE,
-        file = "output/isr_inertia_probe_map.csv")
+        file = "output/isr_inertia_probe_map_random.csv")
 
