66a7bb71c75b751e98f0ed41fcfd0dde51635c06
belledon <mbelledonne@gmail.com>
2022-08-11 18:24:17 -0400

    checkpoint before chain averaging

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 127.0.0.1:8787:8787  --volume $PWD:/home/rstudio/project  -e DISABLE_AUTH=true  5115f3e5a269
+docker run --rm -p 127.0.0.1:8787:8787  --volume $PWD:/home/rstudio/project  -e DISABLE_AUTH=true  rocker/tidyverse
