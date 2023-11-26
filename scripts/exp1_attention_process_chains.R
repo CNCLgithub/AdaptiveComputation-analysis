e57e8efd0dfc32b3087ccd8cf73c854debbde014
belledon <mbelledonne@gmail.com>
2023-11-25 21:17:19 -0500

    changed to

--- scripts/exp1_attention_process_chains.R
+++ scripts/exp1_attention_process_chains.R
@@ -8,6 +8,7 @@ library(slider)
 
 ############################# hyper parameters #################################
 model_smoothing = 48.0 # 12 frames per sd in gaussian kernel; 
+window = 16L
 min_sensitivity = -200
 
 ############################### load raw data ##################################
@@ -33,49 +34,40 @@ dnd_predictions <- read_csv("project/data/exp2/exp2_probes_adaptive_computation_
   rename(dist_to_nd = d_ic)
 
 ############################# smoothing kernel##################################
-smoothed_df <- model_att %>%
+
+averaged_chains <- model_att %>%
   # clean up -Inf sensitivity values
   # mutate(sensitivity = ifelse(is.infinite(sensitivity),
   #                             min_sensitivity,
   #                             sensitivity)) %>%
-  group_by(chain, scene, tracker) %>%
+  group_by(scene, tracker, frame) %>%
+  select(-chain) %>%
+  summarise(across(.fns = list(mean = mean)))
+
+smoothed_df <- averaged_chains %>%
+  group_by(scene, tracker) %>%
   # add smoothing
   nest_by() %>%
-  mutate(att_xy = list(with(data,
-                            ksmooth(frame, importance, kernel = "normal",
-                                    bandwidth = model_smoothing))),
-       cycles_xy = list(with(data,
-                               ksmooth(frame, cycles, kernel = "normal",
-                                       bandwidth = model_smoothing))),
-       pred_xs = list(with(data,
-                           ksmooth(frame, pred_x, kernel = "normal",
-                                   bandwidth = model_smoothing))),
-       pred_ys = list(with(data,
-                           ksmooth(frame, pred_y, kernel = "normal",
-                                   bandwidth = model_smoothing))),
-       
+  mutate(importance_smoothed = list(with(data,
+                                         slide_vec(importance_mean, mean, 
+                                                   .before = window,
+                                                   .after = window))),
+         # cycles_xy = list(with(data,
+         #                       ksmooth(frame, cycles, kernel = "normal",
+         #                               bandwidth = model_smoothing))),
+         # pred_xs = list(with(data,
+         #                     ksmooth(frame, pred_x, kernel = "normal",
+         #                             bandwidth = model_smoothing))),
+         # pred_ys = list(with(data,
+         #                     ksmooth(frame, pred_y, kernel = "normal",
+         #                             bandwidth = model_smoothing))),
+         
          # sens_xy = list(with(data,
          #                     ksmooth(frame, sensitivity, kernel = "normal",
          #                             bandwidth = model_smoothing))),
-       ) %>%
-  mutate(importance_smoothed = list(att_xy$y),
-         cycles_smoothed = list(cycles_xy$y),
-         # sensitivity_smoothed = list(sens_xy$y),
-         pred_x_smoothed = list(pred_xs$y),
-         pred_y_smoothed = list(pred_ys$y),
-         ) %>%
-  unnest(cols = c(data, 
-                  importance_smoothed, 
-                  cycles_smoothed, 
-                  # sensitivity_smoothed,
-                  pred_x_smoothed, pred_y_smoothed,
-                  )) %>%
-  dplyr::select(-c(att_xy, 
-                   cycles_xy, 
-                   # sens_xy,
-                   pred_xs,
-                   pred_ys,
-                   )) %>%
+  ) %>%
+  # select(-c(data)) %>%
+  unnest(cols = c(data, importance_smoothed)) %>%
   ungroup()
 
 # extract probed frames
@@ -83,24 +75,24 @@ smoothed_df <- probe_timings %>%
   left_join(smoothed_df, by = c("scene", "frame"))
 
 centroids <- smoothed_df %>%
-  group_by(scene, chain, frame) %>%
-  summarise(total_cycles = sum(cycles),
-            weighted_x = sum(pred_x * importance_smoothed),
-            weighted_y = sum(pred_y * importance_smoothed),
-            geo_x = mean(pred_x),
-            geo_y = mean(pred_y)) %>%
+  group_by(scene, frame) %>%
+  summarise(total_cycles = sum(cycles_mean),
+            weighted_x = sum(pred_x_mean * importance_smoothed),
+            weighted_y = sum(pred_y_mean * importance_smoothed),
+            geo_x = mean(pred_x_mean),
+            geo_y = mean(pred_y_mean)) %>%
   ungroup()
 
 probe_positions <- smoothed_df %>%
   filter(tracker == probed_tracker) %>%
-  rename(probe_x = pred_x, 
-         probe_y = pred_y) %>%
-  select(chain, scene, epoch, probe_x, probe_y)
+  rename(probe_x = pred_x_mean, 
+         probe_y = pred_y_mean) %>%
+  select(scene, epoch, probe_x, probe_y)
 
 importance_weighted <- smoothed_df %>%
-  select(chain, scene, epoch, tracker, pred_x, pred_y, importance_smoothed) %>%
-  left_join(probe_positions, by = c("chain", "scene", "epoch")) %>%
-  mutate(dist_to_probe = sqrt((probe_x - pred_x)^2 + (probe_y - pred_y)^2))
+  select(scene, epoch, tracker, pred_x_mean, pred_y_mean, importance_smoothed) %>%
+  left_join(probe_positions, by = c("scene", "epoch")) %>%
+  mutate(dist_to_probe = sqrt((probe_x - pred_x_mean)^2 + (probe_y - pred_y_mean)^2))
 
 probe_dist_w = max(importance_weighted$dist_to_probe)
 
@@ -113,16 +105,13 @@ importance_weighted <- importance_weighted %>%
 
 # add distance to weighted and unweighted tracker means
 result <- smoothed_df %>%
-  left_join(centroids, by = c("chain", "scene", "frame")) %>%
-  left_join(importance_weighted, by = c("chain", "scene", "epoch", "tracker")) %>%
-  mutate(a3_centroid = sqrt((weighted_x - pred_x)^2 + (weighted_y - pred_y)^2),
-         geo_centroid = sqrt((geo_x - pred_x)^2 + (geo_y - pred_y)^2),
-         dist_to_center = sqrt(pred_x^2 + pred_y^2),
+  left_join(centroids, by = c("scene", "frame")) %>%
+  left_join(importance_weighted, by = c("scene", "epoch", "tracker")) %>%
+  mutate(a3_centroid = sqrt((weighted_x - pred_x_mean)^2 + (weighted_y - pred_y_mean)^2),
+         geo_centroid = sqrt((geo_x - pred_x_mean)^2 + (geo_y - pred_y_mean)^2),
+         dist_to_center = sqrt(pred_x_mean^2 + pred_y_mean^2),
   ) %>%
   group_by(scene, frame, tracker, probed_tracker, epoch) %>%
-  # average across chains
-  summarise(across(-chain, list(mu = mean,
-                                sd = sd))) %>%
   ungroup() %>%
   left_join(dnd_predictions)
 
@@ -132,7 +121,7 @@ result %>%
   filter(scene <= 5) %>%
   ungroup() %>%
   ggplot(aes(x = frame)) +
-  geom_point(aes(y = a3_centroid_mu, color = factor(tracker))) +
+  geom_point(aes(y = a3_centroid, color = factor(tracker))) +
   facet_grid(rows = vars(scene))
 
 
