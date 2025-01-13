d9878eaa18e3fb276e45bed1d312b5f3f6e51536
belledon <mbelledonne@gmail.com>
2025-01-13 16:19:11 -0500

    added gridsearch and task-relevance scripts

--- scripts/process_chains.R
+++ scripts/process_chains.R
@@ -13,7 +13,7 @@ process_chains <- function(probe_path,
                            att_path,
                            dnd_path) {
   
-  ############################### load raw data ##################################
+  ############################## load raw data #################################
   
   probe_timings <- read_csv(probe_path) %>%
     select(-contains("att")) %>%
@@ -34,7 +34,7 @@ process_chains <- function(probe_path,
     select(scene, frame, d_ic) %>%
     rename(dist_to_nd = d_ic)
   
-  ############################# smoothing kernel##################################
+  ############################ smoothing kernel#################################
   
   
   smoothed_df <- model_att %>%
@@ -134,6 +134,98 @@ process_chains <- function(probe_path,
 
 }
 
+process_grid_chains <- function(probe_path, att_path) {
+  
+  ############################## load raw data #################################
+  
+  # attention traces; processed from `mot/scripts/analysis/aggregate_chains.jl`
+  # scene, frame, tracker, cycles, chain, ...,
+  model_att <- read_csv(att_path)
+  
+  probe_timings <- read_csv(probe_path) %>%
+    select(-contains("att")) %>%
+    filter(scene <= 40) %>%
+    group_by(scene) %>%
+    mutate(epoch = dense_rank(frame)) %>%
+    ungroup() %>%
+    rename(probed_tracker = tracker)
+ 
+  ############################ smoothing kernel#################################
+  
+  
+  smoothed_df <- model_att %>%
+    group_by(chain, scene, tracker) %>%
+    nest_by() %>%
+    mutate(att_xy = list(with(data,
+                              ksmooth(frame, importance, kernel = "normal",
+                                      bandwidth = model_smoothing))),
+           cycles_xy = list(with(data,
+                                 ksmooth(frame, cycles, kernel = "normal",
+                                         bandwidth = model_smoothing))),
+           pred_xs = list(with(data,
+                               ksmooth(frame, pred_x, kernel = "normal",
+                                       bandwidth = model_smoothing))),
+           pred_ys = list(with(data,
+                               ksmooth(frame, pred_y, kernel = "normal",
+                                       bandwidth = model_smoothing))),
+           
+    ) %>%
+    mutate(importance_smoothed = list(att_xy$y),
+           cycles_smoothed = list(cycles_xy$y),
+           pred_x_smoothed = list(pred_xs$y),
+           pred_y_smoothed = list(pred_ys$y),
+    ) %>%
+    unnest(cols = c(data,
+                    importance_smoothed,
+                    cycles_smoothed,
+                    pred_x_smoothed, pred_y_smoothed,
+    )) %>%
+    dplyr::select(-c(att_xy,
+                     cycles_xy,
+                     pred_xs,
+                     pred_ys,
+    )) %>%
+    ungroup()
+  
+  # select probed frames
+  smoothed_df <- probe_timings %>%
+    left_join(smoothed_df, by = c("scene", "frame"))
+  
+  # centroids of all tracked objects (targets)
+  centroids <- smoothed_df %>%
+    group_by(scene, chain, frame) %>%
+    summarise(total_cycles = sum(cycles),
+              # Target center (unweighted)
+              tot_x = mean(pred_x_smoothed),
+              tot_y = mean(pred_y_smoothed),
+              # Attention weighted 
+              tot_att_x = sum(pred_x_smoothed * importance_smoothed),
+              tot_att_y = sum(pred_y_smoothed * importance_smoothed),
+    ) %>%
+    ungroup()
+  
+  # positions of probed object
+  probe_positions <- smoothed_df %>%
+    filter(tracker == probed_tracker) %>%
+    rename(probe_x = pred_x, 
+           probe_y = pred_y) %>%
+    select(chain, scene, epoch, probe_x, probe_y)
+  
+  # add distance to weighted and unweighted tracker means
+  result <- smoothed_df %>%
+    left_join(centroids, by = c("chain", "scene", "frame")) %>%
+    # Compute measures for AC-centroid, Target center, and ECC
+    mutate(
+      a3_centroid = sqrt((tot_att_x - pred_x)^2 + (tot_att_y - pred_y)^2),
+    ) %>%
+    group_by(scene, frame, tracker, probed_tracker, epoch) %>%
+    # average across chains
+    summarise(across(-chain, mean)) %>%
+    ungroup()
+  
+  return(result)
+  
+}
 # 
 # 
 # 
